0c03c00d8cf2f218008464ff820869d5dfed285a
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-11 08:47:41 +0100

    reducer set up

---
+++ smurfs/src/states/reducers.js
@@ -0,0 +1,18 @@
+import * as types from "./actionTypes";
+
+const initialCount = 0;
+
+export function countReducer(state = initialCount, action) {
+  switch (action.type) {
+    case types.INCREMENT:
+      return state + 1;
+
+    case types.DECREMENT:
+      return state - 1;
+
+    case types.RESET:
+      return 0;
+    default:
+      return state;
+  }
+};
